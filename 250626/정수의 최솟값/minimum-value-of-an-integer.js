91f9731d3d98253725065ee2498d90504598dba7
hxe_zu <123932695+hxezu@users.noreply.github.com>
2025-06-26 00:37:35 +0900

    Title: 정수의 최솟값, Time: 169ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250626/정수의 최솟값/minimum-value-of-an-integer.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let map = input[0].split(" ").map(Number);
+
+map.sort()
+
+console.log(map[0])
+
+// Please Write your code here.
