8368a20f0038e3e322e3e97e016b5ec62ddbcb23
hxe_zu <123932695+hxezu@users.noreply.github.com>
2025-06-30 00:08:43 +0900

    Title: 007, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250630/007/007.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+class Secret {
+    constructor(s_code, m_point,time){
+        this.s_code = s_code;
+        this.m_point = m_point;
+        this.time = time
+    }
+}
+const [s_code, m_point, time] = input[0].split(' ');
+
+const secret = new Secret(s_code, m_point, time)
+console.log("secret code : "+secret.s_code)
+console.log("meeting point : "+secret.m_point)
+console.log("time : "+secret.time)
