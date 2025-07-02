ae55f2191f07351b5b85362cc54bbc355091531c
hxe_zu <123932695+hxezu@users.noreply.github.com>
2025-07-02 23:28:11 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 184ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250702/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+// Please Write your code here.
+
+function sum(n){
+    if(n===1){
+        return 1;
+    }
+    return sum(n-1)+n;
+}
+console.log(sum(n))
