5b2d4c2ff10abd59cf7c12e038b459019a2ef9c4
hxe_zu <123932695+hxezu@users.noreply.github.com>
2025-07-08 21:36:24 +0900

    Title: 십진수로 변환하기, Time: 195ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250708/십진수로 변환하기/convert-to-decimal.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const binaryStr = input[0].split('');
+let num = 0;
+
+for(let i=0; i<binaryStr.length; i++){
+    num = num*2 + Number(binaryStr[i])
+}
+
+console.log(num)
