3ea6d5a8d4469a0398b1b63800bc4bc2a5ec5338
hxe_zu <123932695+hxezu@users.noreply.github.com>
2025-08-03 21:25:42 +0900

    Title: 사각형의 총 넓이 2, Time: 203ms, Memory: 15MB, Status: Passed - Codetree

--- 250803/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
+++ 250803/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -10,7 +10,7 @@ let answer = 0;
 function colorRects([x1,y1,x2,y2]){
     for(let i=x1; i<x2; i++){
         for(let j=y1; j<y2; j++){
-            const key = `${i}${j}`
+            const key = `${i}:${j}`
             if(!answerSet.has(key)){
                 answerSet.add(key)
                 answer++;
