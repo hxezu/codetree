b1fdb3d2dfcf9e9e4e83d563d28f288f6aefd245
hxe_zu <123932695+hxezu@users.noreply.github.com>
2025-08-16 18:33:29 +0900

    Title: 개인정보, Time: 201ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250816/개인정보/personal-info.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+class Student{
+    constructor(name, height, weight){
+        this.name = name;
+        this.height = height;
+        this.weight = weight
+    }
+}
+
+const students = [];
+for (let i = 0; i < 5; i++) {
+  const [name, heightStr, weightStr] = input[i].split(' ');
+  const height = Number(heightStr);
+  const weight = Number(weightStr);
+  students.push(new Student(name, height, weight));
+}
+
+// Please Write your code here.
+function sortByName(students){
+    console.log("name")
+    students
+    .slice()
+    .sort((a,b)=> a.name.localeCompare(b.name))
+    .forEach(s=> console.log(s.name +" "+s.height+" "+s.weight))
+}
+sortByName(students)
+console.log("")
+
+function sortByHeight(students){
+    console.log("height")
+    students
+    .slice()
+    .sort((a,b)=>b.height-a.height)
+    .forEach(s=> console.log(s.name +" "+s.height+" "+s.weight))
+}
+sortByHeight(students)
