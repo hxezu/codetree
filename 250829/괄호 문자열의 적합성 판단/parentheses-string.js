6d77b77c5a329109858d153f98b89e943ce26e00
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-08-29 14:45:35 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 185ms, Memory: 15MB, Status: Passed - Codetree

--- 250829/괄호 문자열의 적합성 판단/parentheses-string.js
+++ 250829/괄호 문자열의 적합성 판단/parentheses-string.js
@@ -4,10 +4,19 @@ const bracketStr = input[0];
 // Please write your code here.
 
 const stack = [];
-
+let isValid = true
 
 for(let x of bracketStr){
-    x==="("?stack.push(x): stack.pop()
+    if(x==="("){
+        stack.push(x)
+    }else if(x===")"){
+        if(stack.length===0){
+            isValid = false
+            break
+        }else{
+            stack.pop()
+        }
+    }
 }
 
-console.log(stack.length===0? "Yes":"No")
+console.log(stack.length===0 && isValid? "Yes":"No")
