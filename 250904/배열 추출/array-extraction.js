e873ed8fe834eb6911281524dfdf512a52908c8c
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-04 20:25:42 +0900

    Title: 배열 추출, Time: 637ms, Memory: 46MB, Status: Passed - Codetree

--- 250904/배열 추출/array-extraction.js
+++ 250904/배열 추출/array-extraction.js
@@ -1,49 +1,20 @@
+const Heap = require("collections/heap")
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
-
-class Node{
-    constructor(data){
-        this.data = data;
-        this.next = null
-    }
-}
-
-class LinkedList{
-    constructor(){
-        this.head = null
-        this.size = 0
-    }
-
-    push(value){
-        const newNode = new Node(value)
-        if(!this.size || this.head.data < value){
-            newNode.next = this.head
-            this.head = newNode
-        }else{
-            let curNode = this.head
-            while( curNode.next && curNode.next.data >= value ){
-                curNode = curNode.next
-            }
-            newNode.next = curNode.next
-            curNode.next = newNode
-        }
-        this.size++
-    }
-
-    pop(){
-        if(!this.size) return 0
-        const value = this.head.data
-        this.head = this.head.next
-        this.size--
-        return value
-    }
-}
+const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const n = Number(input[0]);
 const arr = input.slice(1, 1 + n).map(Number);
-const nodes = new LinkedList()
+const pq = new Heap()
 
-// Please Write your code here.
-for(const num of arr){
-    num ? nodes.push(num) : console.log(nodes.pop())
-}
+arr.forEach(x=>{
+    if(x!==0){
+        pq.push(x)
+    }
+    else{
+        if(!pq.length){
+            console.log(0)
+        }else{
+            console.log(pq.pop())
+        }
+    }
+})
