cbeefd4d796177c412f6d792b4f941d691119ce1
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-04 21:58:25 +0900

    Title: 마지막으로 남은 숫자, Time: 233ms, Memory: 20MB, Status: Passed - Codetree

--- 250904/마지막으로 남은 숫자/last-remaining-number.js
+++ 250904/마지막으로 남은 숫자/last-remaining-number.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input[1].split(" ").map(Number);
-const pq = new Heap([], null, (a,b)=>b-a)
+const pq = new Heap()
 
 arr.forEach(num=>pq.push(num))
 
-while(pq.size>=2){
+while(pq.length>=2){
     const value = pq.pop() - pq.pop()
     if(value >0) pq.push(value)
 }
 
-console.log(pq.size ? -1 : pq.peek())
+console.log(pq.length ? pq.peek():-1 )
