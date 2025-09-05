c528c78e2e11c88cdb37e64231d6a8a4d7532a1f
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-05 20:50:57 +0900

    Title: 서로 다른 수의 개수, Time: 83ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250905/서로 다른 수의 개수/distinct-numbers.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].trim().split(' ').map(Number);
+
+// Please Write your code here.
+const newset = new Set(arr)
+console.log(newset.size)
