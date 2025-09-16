e2777bb09a1b76cf466a96423b7a11a8e5e43e79
HYEJU HYUN <123932695+hxezu@users.noreply.github.com>
2025-09-16 14:27:47 +0900

    Title: 최고의 13위치, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250916/최고의 13위치/best-place-of-13.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1).map(line => line.trim().split(' ').map(Number));
+
+// Please Write your code here.
+let maxCnt = 0
+
+for(let i=0; i<n ; i++){
+    for(let j=0; j<n-2; j++){
+        maxCnt = Math.max(maxCnt, arr[i][j]+arr[i][j+1]+arr[i][j+2])
+    }
+}
+
+console.log(maxCnt)
