167f277bb863405785feb5af23bb500ecb3fab32
Hyeju Hyun <123932695+hxezu@users.noreply.github.com>
2025-10-04 21:55:16 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251004/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const grid = input.slice(1, n + 1).map(row => row.split(' ').map(Number));
+
+const visited = Array.from({length:n}, ()=>Array(n).fill(false))
+
+function canGo(x,y){
+    if(x<0 || x>=n || y<0 || y>=n) return false
+    if(visited[x][y] || grid[x][y]===0) return false
+    return true
+}
+
+let reachable = 0
+
+function dfs(x,y){
+    if(x===n-1 && y===n-1){
+        reachable = 1
+        return 
+    }
+
+    visited[x][y] = true
+    const dx = [1,0]
+    const dy = [0,1]
+
+    for(let i=0; i<dx.length; i++){
+        const newX = x + dx[i]
+        const newY = y + dy[i]
+
+        if(canGo(newX, newY)){
+            dfs(newX, newY)
+        }
+    }
+}
+
+dfs(0,0)
+console.log(reachable)
