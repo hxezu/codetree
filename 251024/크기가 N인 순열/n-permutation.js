b353a67143c5427088b5539b187c328dd2bcca3a
Hyeju Hyun <123932695+hxezu@users.noreply.github.com>
2025-10-24 14:16:46 +0900

    Title: 크기가 N인 순열, Time: 122ms, Memory: 24MB, Status: Passed - Codetree

---
+++ 251024/크기가 N인 순열/n-permutation.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const result = []
+const visited = Array(n+1).fill(false)
+
+function dfs(path){
+    if(path.length === n){
+        result.push(path.join(" "))
+        return;
+    }
+
+    for(let j=1; j<=n; j++){
+        if(!visited[j]){
+            visited[j] = true
+            dfs([...path, j])
+            visited[j] = false
+        }
+    }
+}
+
+dfs([])
+console.log(result.join("\n"))
